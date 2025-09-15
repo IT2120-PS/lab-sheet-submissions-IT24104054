ba66521feaddf6e4364b23b18d5740b163404799
IT24104054 <it24104054@my.sliit.lk>
2025-09-15 09:36:35 +0530

    Lab 6

---
+++ Lab Sheet 06/IT24104054_Chemini K.A.D.S._lab06.R
@@ -0,0 +1,33 @@
+setwd("C:/Users/Bursar/Desktop/IT24104054")
+
+
+#Question 1
+#Part 1 
+#Binomial Distribution
+n <- 50
+p <- 0.85
+
+
+#Part 2 
+#P(X >= 47) = 1-P(x <= 46)
+1 - pbinom(46, n, p, lower.tail = TRUE)
+#pbinom(46, n, p, lower.tail = FALSE)
+
+
+
+#Question 2
+
+
+#Part 1
+#Number of calls a call center recieves per hour
+
+
+
+#Part 2 
+#Poisson Distribution
+#Lambda = 12
+
+
+#Part 3
+lambda <- 12
+dpois(15, lambda)
